107adec4b761e1a437ef60776685215a475af4a0
hrawat_xavient <harshrock2010@gmail.com>
2018-02-13 18:51:40 +0530

    added apps

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include, path
+from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('webapp/', include('webapp.urls'))
+
 ]
